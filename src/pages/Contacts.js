5d68031401e87cc51a2a8f5591a565b9bd25584a
Hardingendaniel <danieae@stud.ntnu.no>
2025-02-02 13:42:00 +0100

    Fixed styling issue with link under picture and content

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -18,41 +18,49 @@ const Contact = () => {
     return (
         <div className='crud'>
             <div className='container-contact'>
-                <div className='personal info'>
+                <div className='personal-info'>
                     <div className='personal-paramters'>
+                        <div className='personal-object'>
                         <p className='person-text'>Namn:</p>
-                        <p>{person.firstName}</p>
-                    </div>
-                    <div className='personal-paramters'>
-                        <p className='person-text'>Etternamn:</p>
-                        <p>{person.lastName}</p>
-                    </div>
-                
-                    <div className='personal-paramters'>
-                        <p className='person-text'>e-post:</p>
-                        <p>{person.email}</p>
-                    </div>
-                    <div className='personal-paramters'>
-                        <p className='person-text'>Alder:</p>
-                        <p>{person.age}</p>
-                    </div>
-                   
-                </div>
-                 <div>
-                    <img src={Daniel} alt='Personal' className='picture' />
-                </div>
-               
-                <div>
-                <div className='linkdiv'>
-                            <Link to='https://github.com/Hardingendaniel?tab=repositories' target='blank'>
-                                <img src={git} alt='Personal' className='linkpicture'></img>
-                            </Link>
-                            <Link to='https://www.linkedin.com/in/daniel-eik%C3%A5s-99b481235/' target='blank'>
-                                <img src={linkedin} alt='Personal' className='linkpicture'></img>
-                            </Link>
+                        <p className='person-text'>{person.firstName}</p>
                         </div>
+                       
+                        <div className='personal-object'>
+                            <p className='person-text'>Etternamn:</p>
+                            <p className='person-text'>{person.lastName}</p>
+                        </div>
+
+                        <div className='personal-object'>
+                            <p className='person-text'>e-post:</p>
+                            <p className='person-text'>{person.email}</p>
+                        </div>
+                        <div className='personal-object'>
+                            <p className='person-text'>Alder:</p>
+                            <p className='person-text'>{person.age}</p>
+                        </div>
+
+                        <div className='personal-object'>
+                            <p className='person-text'>Telefon::</p>
+                            <p className='person-text'>{person.phoneNumber}</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src={Daniel} alt='Personal' className='picture' />
+                    </div>
+
+                </div>
+
+                <div className='linkdiv-contact'>
+                    <Link to='https://github.com/Hardingendaniel?tab=repositories' target='blank'>
+                        <img src={git} alt='Personal' className='linkpicture'></img>
+                    </Link>
+                    <Link to='https://www.linkedin.com/in/daniel-eik%C3%A5s-99b481235/' target='blank'>
+                        <img src={linkedin} alt='Personal' className='linkpicture'></img>
+                    </Link>
                 </div>
             </div>
+            
         </div>
     )
 };
